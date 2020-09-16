afa4c34f283296be04bd4992af38d9e7dbe04d90
Joseph Bliss <josephblister@gmail.com>
2020-09-16 04:20:19 +0200

    Completed module 1

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -9,12 +9,11 @@ for expense in expenses.list:
     spending_categories.append(expense.category)
 
 spending_counter = collections.Counter(spending_categories)
-top_5 = spending_counter.most_common(5)
+top5 = spending_counter.most_common(5)
 
-(categories,count) = zip(*top_5)
-print(count)
+(categories,count) = zip(*top5)
 
 fig, ax = plt.subplots()
 ax.bar(categories,count)
-ax.set_title('# of purchase by category')
+ax.set_title('# of Purchases by Category')
 plt.show()
